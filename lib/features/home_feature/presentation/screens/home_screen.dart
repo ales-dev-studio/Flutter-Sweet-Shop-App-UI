fe1ee1f0152d21ba52abefbd8aad09c24999775f
ales <ales.dev.studio@gmail.com>
2025-07-29 02:19:31 +0330

    Implement splash screen

---
+++ lib/features/home_feature/presentation/screens/home_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_sweet_shop_app_ui/core/widgets/app_scaffold.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AppScaffold();
+  }
+}
--- lib/features/home_feature/presentation/screens/splash_screen.dart
+++ lib/features/home_feature/presentation/screens/splash_screen.dart
@@ -1,5 +1,13 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_sweet_shop_app_ui/core/theme/theme.dart';
+import 'package:flutter_sweet_shop_app_ui/core/utils/app_navigator.dart';
+import 'package:flutter_sweet_shop_app_ui/core/utils/check_device_size.dart';
 import 'package:flutter_sweet_shop_app_ui/core/widgets/app_scaffold.dart';
+import 'package:flutter_sweet_shop_app_ui/features/home_feature/presentation/screens/home_screen.dart';
+
+import '../../../../core/gen/assets.gen.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -10,10 +18,31 @@ class SplashScreen extends StatefulWidget {
 
 class _SplashScreenState extends State<SplashScreen> {
   @override
+  void initState() {
+    super.initState();
+    Timer(Duration(milliseconds: 1500), () {
+      appPushReplacement(context, HomeScreen());
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final colors = context.theme.appColors;
     return AppScaffold(
-      appBar: AppBar(
-        title: Text('test'),
+      backgroundColor: colors.brownExtraLight,
+      padding: EdgeInsets.zero,
+      safeAreaTop: false,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Assets.images.splashHeader.image(),
+          Assets.images.logo.image(
+            width: checkVerySmallDeviceSize(context) ? 290 : 390,
+          ),
+          Assets.images.cake.image(
+            width: checkVerySmallDeviceSize(context) ? 205 : 305,
+          ),
+        ],
       ),
     );
   }
