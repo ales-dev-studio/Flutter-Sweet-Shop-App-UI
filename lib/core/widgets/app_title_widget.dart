e748a2995e5a4434447a7a026acd479cb03de771
ales <ales.dev.studio@gmail.com>
2025-07-31 04:16:39 +0330

    Implement the banner slider widget

---
+++ lib/core/widgets/app_title_widget.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_sweet_shop_app_ui/core/theme/theme.dart';
+import 'package:flutter_sweet_shop_app_ui/core/widgets/app_text_button.dart';
+
+import '../gen/assets.gen.dart';
+import '../theme/dimens.dart';
+
+class AppTitleWidget extends StatelessWidget {
+  const AppTitleWidget({
+    super.key,
+    required this.title,
+    this.thumbnailPath,
+    required this.onPressed,
+  });
+
+  final String title;
+  final String? thumbnailPath;
+  final GestureTapCallback onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: Dimens.largePadding),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            title,
+            style: context.theme.appTypography.titleSmall.copyWith(
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          AppTextButton(
+            onPressed: onPressed,
+            title: 'See all',
+            iconPath: Assets.icons.arrowRight1,
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/features/home_feature/presentation/widgets/tabs/home_tab.dart
+++ lib/features/home_feature/presentation/widgets/tabs/home_tab.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_sweet_shop_app_ui/features/home_feature/presentation/widgets/banner_slider_widget.dart';
+
+import '../../../../../core/widgets/app_title_widget.dart';
 
 class HomeTab extends StatelessWidget {
   const HomeTab({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text('HomeTab'));
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          AppTitleWidget(onPressed: () {}, title: 'Special offers'),
+          BannerSliderWidget(),
+        ],
+      ),
+    );
   }
 }
