46cf9b59c0fb808ec6b23684b5d5a9f40845b73c
ales <ales.dev.studio@gmail.com>
2025-08-04 19:47:26 +0330

    Implement the categories list widget

--- lib/features/home_feature/presentation/widgets/tabs/home_tab.dart
+++ lib/features/home_feature/presentation/widgets/tabs/home_tab.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_sweet_shop_app_ui/features/home_feature/presentation/widgets/banner_slider_widget.dart';
 
 import '../../../../../core/widgets/app_title_widget.dart';
+import '../categories_list.dart';
 
 class HomeTab extends StatelessWidget {
   const HomeTab({super.key});
@@ -13,6 +14,8 @@ class HomeTab extends StatelessWidget {
         children: [
           AppTitleWidget(onPressed: () {}, title: 'Special offers'),
           BannerSliderWidget(),
+          AppTitleWidget(onPressed: () {}, title: 'Categories'),
+          CategoriesList(),
         ],
       ),
     );
