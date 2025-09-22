b84851f39e90338c422c18bb228466c00448c626
ales <ales.dev.studio@gmail.com>
2025-09-23 01:45:04 +0330

    Implement orders screen

--- lib/core/widgets/general_app_bar.dart
+++ lib/core/widgets/general_app_bar.dart
@@ -6,30 +6,46 @@ import '../theme/dimens.dart';
 import 'app_bordered_icon_button.dart';
 
 class GeneralAppBar extends StatelessWidget implements PreferredSizeWidget {
-  const GeneralAppBar({super.key, required this.title, this.actions});
+  const GeneralAppBar({
+    super.key,
+    required this.title,
+    this.actions,
+    this.showBackIcon = true,
+    this.bottom,
+    this.height,
+  });
 
   final String title;
   final List<Widget>? actions;
+  final bool showBackIcon;
+  final PreferredSizeWidget? bottom;
+  final double? height;
 
   @override
   Widget build(BuildContext context) {
     return AppBar(
       actions: actions,
       title: Text(title),
-      leading: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: Dimens.largePadding),
-        child: AppBorderedIconButton(
-          iconPath: Assets.icons.arrowLeft,
-          onPressed: () {
-            appPop(context);
-          },
-        ),
-      ),
+      leading:
+          showBackIcon
+              ? Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: Dimens.largePadding,
+                ),
+                child: AppBorderedIconButton(
+                  iconPath: Assets.icons.arrowLeft,
+                  onPressed: () {
+                    appPop(context);
+                  },
+                ),
+              )
+              : null,
       leadingWidth: 90.0,
+      bottom: bottom,
     );
   }
 
   @override
   Size get preferredSize =>
-      Size.fromHeight(AppBar().preferredSize.height + 16.0);
+      Size.fromHeight(height ?? AppBar().preferredSize.height + 16.0);
 }
