f1ee5d2f42c4c4239e737d4da2d347e523c88d24
ales <ales.dev.studio@gmail.com>
2025-10-18 18:05:56 +0330

    Fix the splash screen bugs

--- lib/features/home_feature/presentation/screens/splash_screen.dart
+++ lib/features/home_feature/presentation/screens/splash_screen.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_sweet_shop_app_ui/core/theme/dimens.dart';
 import 'package:flutter_sweet_shop_app_ui/core/theme/theme.dart';
 import 'package:flutter_sweet_shop_app_ui/core/utils/app_navigator.dart';
 import 'package:flutter_sweet_shop_app_ui/core/utils/check_device_size.dart';
@@ -34,12 +35,14 @@ class _SplashScreenState extends State<SplashScreen> {
       safeAreaTop: false,
       body: SingleChildScrollView(
         child: Column(
+          spacing: Dimens.largePadding,
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
             Assets.images.splashHeader.image(),
             Assets.images.logo.image(
               width: checkVerySmallDeviceSize(context) ? 290 : 390,
             ),
+            SizedBox(height: Dimens.largePadding),
             Assets.images.cake.image(
               width: checkVerySmallDeviceSize(context) ? 205 : 305,
             ),
